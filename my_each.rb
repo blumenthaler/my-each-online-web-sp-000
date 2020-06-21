2adbf83089424a0240db4a95db45b52b3f2b559c
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-21 23:15:48 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -11,30 +11,3 @@ collection = [1, 2, 3, 4]
 my_each(collection) do |i|
   puts i
 end
-
-
-def hello_t(array)
-  if block_given?
-    i = 0
-    while i < array.length
-      yield array[i]
-        i += 1
-    end
-    array
-  else
-    puts "Hey! No block was given!"
-  end
-end
-
-collection = [1, 2, 3, 4]
-my_each(collection) do |i|
-  puts i
-end
-
-
-# call your method here!
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
